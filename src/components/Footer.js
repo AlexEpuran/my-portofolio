bc758286ce1cdbbad0051250a181f1d174164149
AlexEpu <epuranalex89@gmail.com>
2020-10-09 13:02:04 +0300

    added some classes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,8 @@ import { Link, animateScroll as scroll } from "react-scroll";
 function Footer() {
   return (
     <div className="footer-container" id="plm">
-      
+      <h1 className="footer-text">My Portofolio </h1>
+      <p className="footer-para">&copy; Copyright 2020</p>
     </div>
   );
 }

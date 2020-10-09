fc9910f758d8a1708c2fd19c645b33ea5665e1be
AlexEpu <epuranalex89@gmail.com>
2020-10-09 15:04:40 +0300

    about section work

--- src/components/pages/AboutSection.js
+++ src/components/pages/AboutSection.js
@@ -1,11 +1,20 @@
 import React from "react";
-import './AboutSection.css'
+import "./AboutSection.css";
 
 export default function AboutSection({ id }) {
   return (
-      <div className="about-section-main" id={id}>
-        <h1>ABOUT</h1>
-        <div></div>
+    <div className="about-section-main" id={id}>
+      <div className="about-text-container">
+        <h1 className="about-main-text">
+          About <span className="about-span">Me</span>
+        </h1>
       </div>
+      <div className="picture-second-text-container">
+        <div className="picture-container"></div>
+        <div className="second-text-container">
+          <h1>TEST</h1>
+        </div>
+      </div>
+    </div>
   );
 }
